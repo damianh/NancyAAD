587e0229920b349a02af616b6db1ed954ea2d5b3
Damian Hickey <dhickey@gmail.com>
2014-01-03 23:42:52 +0000

    First commit of awesomeness.

---
+++ src/NancyAAD.Client/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace NancyAAD.Client
+{
+    using System;
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using Microsoft.WindowsAzure.ActiveDirectory.Authentication;
+
+    internal class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Client ready.");
+            Console.WriteLine("Press any key to invoke the service");
+            Console.WriteLine("Press ESC to terminate");
+            ConsoleKeyInfo consoleKeyInfo;
+
+            var authenticationContext = new AuthenticationContext("https://login.windows.net/contoso7.onmicrosoft.com");
+
+            do
+            {
+                consoleKeyInfo = Console.ReadKey(true);
+                // get the access token
+                AuthenticationResult authenticationResult = authenticationContext.AcquireToken(
+                    "https://contoso7.onmicrosoft.com/RichAPI",
+                    "be182811-9d0b-45b2-9ffa-52ede2a12230",
+                    "http://whatevah");
+                // invoke the Nancy API
+                var httpClient = new HttpClient();
+                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authenticationResult.AccessToken);
+                HttpResponseMessage response = httpClient.GetAsync("http://localhost:9000/api/Values").Result;
+                // display the result
+                if (response.IsSuccessStatusCode)
+                {
+                    string result = response.Content.ReadAsStringAsync().Result;
+                    Console.WriteLine("==> Successfully invoked the service");
+                    Console.WriteLine(result);
+                }
+            } while (consoleKeyInfo.Key != ConsoleKey.Escape);
+        }
+    }
+}
---
+++ src/NancyAAD.Server/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace NancyAAD.Server
+{
+    using System;
+    using System.Security.Claims;
+    using Microsoft.Owin.Hosting;
+    using Microsoft.Owin.Security.ActiveDirectory;
+    using Nancy;
+    using Nancy.Security;
+    using Owin;
+
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (WebApp.Start<Startup>("http://localhost:9000/"))
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Nancy listening at http://localhost:9000/");
+
+                /*// Test call
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync("http://localhost:9000/api/values").Result;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);*/
+
+                Console.ReadLine();
+            }
+        }
+    }
+
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWindowsAzureActiveDirectoryBearerAuthentication(
+                new WindowsAzureActiveDirectoryBearerAuthenticationOptions
+                {
+                    Audience = "https://contoso7.onmicrosoft.com/RichAPI",
+                    Tenant = "contoso7.onmicrosoft.com"
+                })
+                .UseNancy();
+        }
+    }
+
+    public class ValuesModule : NancyModule
+    {
+        public ValuesModule() : base("api")
+        {
+            Get["/values"] = _ =>
+            {
+                ClaimsPrincipal claimsPrincipal = Context.GetAuthenticationManager().User;
+                Console.WriteLine("==>I have been called by {0}", claimsPrincipal.FindFirst(ClaimTypes.Upn));
+                return new[] {"value1", "value2"};
+            };
+        }
+    }
+}
